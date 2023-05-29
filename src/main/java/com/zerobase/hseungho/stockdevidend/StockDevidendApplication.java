a8fbf14d40cf2a05f52f4ad5de77624050da4649
hseungho <hsh47607@naver.com>
2023-05-30 01:34:21 +0900

    init project

---
+++ src/main/java/com/zerobase/hseungho/stockdevidend/StockDevidendApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.hseungho.stockdevidend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDevidendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDevidendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/hseungho/stockdevidend/StockDevidendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.hseungho.stockdevidend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDevidendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
