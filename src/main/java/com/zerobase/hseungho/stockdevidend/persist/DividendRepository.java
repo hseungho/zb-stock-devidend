ad18c006f5a21bee2afab23f17a18e3e73a89e18
hseungho <hsh47607@naver.com>
2023-05-31 02:24:29 +0900

    init entity and repos

---
+++ src/main/java/com/zerobase/hseungho/stockdevidend/persist/DividendRepository.java
@@ -0,0 +1,9 @@
+package com.zerobase.hseungho.stockdevidend.persist;
+
+import com.zerobase.hseungho.stockdevidend.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+}
